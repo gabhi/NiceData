5167362fc887e0b4a2b9c9df185704d19e4a0377
Christian Yenko <chris@jivehealth.com>
2014-03-12 03:27:37 -0500

    Added bootstrap, index landing page
    - Created landing page with bootstrap, will soon fill with all that
    HTML5 goodness.

--- Scarecrow/urls.py
+++ Scarecrow/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Scarecrow.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'ScarecrowApp.views.index',name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- ScarecrowApp/views.py
+++ ScarecrowApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse
+from django.template import loader, Context
 
 # Create your views here.
+def index(request):
+	template = loader.get_template('index.html')
+	context = ({'Context': 'Hi!'})
+	return render(request,'index.html',context)

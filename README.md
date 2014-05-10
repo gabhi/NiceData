b18a12e2a026258414062f0b511edd1f8168b2f2
Christian Yenko <chris@jivehealth.com>
2014-05-10 01:05:16 -0500

    Hosted MVP & Changed Styling
    CHANGELOG
    ———
    - Hosted Minimum Viable Product on Django, featuring the core use cases
    with the exception of export.
    - Removed MPLTools dependency and styling, in favor of a custom styling
    by Bicubic. Will customize at a later date.
    - Change README to include link to demo
    TODO
    ———
    - Implement CSV Export
    - Create repo for Quandl CSV to jqGrid formatter
    - Minor aesthetic changes: Enlarge header, change bg image

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-NiceData
+[NiceData](http://glacial-peak-3884.herokuapp.com/ "Link to Demo")
 =========
+![alt tag](https://dl.dropboxusercontent.com/u/45206361/interface_mvp1.png)
+About
+=========
+A small project for quickly creating financial graphics and analyses based on historical data. It pulls timeseries data from Yahoo! Finance, and builds charts using matplotlib with custom styling. 
 
-A small project for quickly creating financial graphics and analyses based on historical data. It pulls timeseries data from Yahoo! Finance, and builds charts using matplotlib. 
+Ticker data displayed using jqGrid and Quandl can be viewed on the side panel, with export feature to be implemented soon.
 
 I decided to include an exact ticker data feature using jqGrid and the Quandl API. I'll provide the option to export to CSV shortly.
 
-This project is currently in development, and is not ready to be hosted.
-
+This project has a MVP that is hosted [here](http://glacial-peak-3884.herokuapp.com/).
 TODO
 =========
 - Implement frontend interface for moving average, maybe bollinger bands.
 - Pull XBRL data for companies and automate spreadsheet creation.
-
-What it looks like
-=========
-![alt tag](https://dl.dropboxusercontent.com/u/45206361/interface_mvp1.png)
+- Implement CSV Export
+- Create repo for Quandl CSV to jqGrid formatter
+- Minor aesthetic changes: Enlarge header, change bg image
--- ScarecrowApp/lib/ScarecrowObjects.py
+++ ScarecrowApp/lib/ScarecrowObjects.py
@@ -1,3 +1,5 @@
+from pylab import *
+
 from matplotlib import pyplot
 from matplotlib.dates import DateFormatter
 import urllib
@@ -146,7 +148,48 @@ class ObservationSeries:
 		canvas = FigureCanvas(savefigure)
 		return canvas
 
-	#Returns a cstring encoded image
+	#Courtesy of Bicubic of messymind.net
+	def rstyle(self,ax): 
+	    """Styles an axes to appear like ggplot2
+	    Must be called after all plot and axis manipulation operations have been carried out (needs to know final tick spacing)
+	    """
+	    #set the style of the major and minor grid lines, filled blocks
+	    ax.grid(True, 'major', color='w', linestyle='-', linewidth=1.4)
+	    ax.grid(True, 'minor', color='0.92', linestyle='-', linewidth=0.7)
+	    ax.patch.set_facecolor('0.85')
+	    ax.set_axisbelow(True)
+	    
+	    #set minor tick spacing to 1/2 of the major ticks
+	    ax.xaxis.set_minor_locator(MultipleLocator( (plt.xticks()[0][1]-plt.xticks()[0][0]) / 2.0 ))
+	    ax.yaxis.set_minor_locator(MultipleLocator( (plt.yticks()[0][1]-plt.yticks()[0][0]) / 2.0 ))
+	    
+	    #remove axis border
+	    for child in ax.get_children():
+	        if isinstance(child, matplotlib.spines.Spine):
+	            child.set_alpha(0)
+	       
+	    #restyle the tick lines
+	    for line in ax.get_xticklines() + ax.get_yticklines():
+	        line.set_markersize(5)
+	        line.set_color("gray")
+	        line.set_markeredgewidth(1.4)
+	    
+	    #remove the minor tick lines    
+	    for line in ax.xaxis.get_ticklines(minor=True) + ax.yaxis.get_ticklines(minor=True):
+	        line.set_markersize(0)
+	    
+	    #only show bottom left ticks, pointing out of axis
+	    rcParams['xtick.direction'] = 'out'
+	    rcParams['ytick.direction'] = 'out'
+	    ax.xaxis.set_ticks_position('bottom')
+	    ax.yaxis.set_ticks_position('left')
+	    
+	    
+	    if ax.legend_ <> None:
+	        lg = ax.legend_
+	        lg.get_frame().set_linewidth(0)
+	        lg.get_frame().set_alpha(0.5)
+	#Returns a cstring encoded image     
 	def plotSeriesCString(self,param0="date",param1="close"): #plot using pyplot
 		#style.use('ggplot') #using ggplot to make nice plots
 		if len(self.dataSet)==0 and param1!="date": #Check to see if there is anything to plot and that y axis is NOT date
@@ -190,13 +233,13 @@ class ObservationSeries:
 		#pathForChart = "tempImg.png"
 		#savefigure.savefig(pathForChart, bbox_inches='tight')
 		#canvas = FigureCanvas(savefigure)
-
+		self.rstyle(subPlot)
 		#New for AJAX - return c string representation
 		sio = cStringIO.StringIO()
 		pyplot.savefig(sio,format='PNG')
 		encoded_img = sio.getvalue().encode('Base64')
 		return encoded_img
-
+	        
 class SObservation:
 	def __init__(self,inDate="N/A",inTicker="N/A",inOpen=-1,inHigh=-1,inLow=-1,inClose=-1,inVol=-1,inAdjClose=-1):
 		self.date=inDate #a date object
